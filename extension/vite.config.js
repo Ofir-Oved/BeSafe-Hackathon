80836f50b9a01d5aac19a2812ee41b68d50753f1
Ofir-Oved <ofir.oved13@gmail.com>
2025-12-21 13:01:27 +0200

    infrastructure resdy + extension runs in chrome

--- extension/vite.config.js
+++ extension/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    cssCodeSplit: true,
+    rollupOptions: {
+      input: {
+        content: "src/content.js",
+        overlay: "src/overlay.jsx",
+      },
+      output: {
+        entryFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+        chunkFileNames: "chunk.[hash].js",
+      },
+    },
+  },
+});
