5620949e729a61f68c3f05b6ecb04791a025bc33
Ofir-Oved <ofir.oved13@gmail.com>
2025-12-22 18:15:49 +0200

    remove leftover DuckContext from template

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router'
-import Home from './pages/HomePage/HomePage';
 import styles from './styles/App.module.css';
 
 import projectLogo from './assets/project-logo.png'
